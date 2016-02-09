b4a3aec6392f81354cdfbf47008c10ea61c91fce
David Sosa Valdes <david.sosa.valdes@gmail.com>
2016-02-09 10:58:04 -0600

    update bundle config instructions

--- config/bundles.php
+++ config/bundles.php
@@ -17,18 +17,17 @@
 |			'route'  => 'admin',
 |		),
 |
-| Note that the "location" is relative to the "bundles" directory.
+| Note that the 'location' is relative to the 'bundles' directory.
 | Now the bundle will be recognized by Codeigniter and will be able
-| to respond to requests beginning with "admin".
+| to respond to requests beginning with 'admin'.
+|
+| Also you can add a 'default' tag and set it TRUE if you want that 
+| bundle be the default request, so insted searching by the route 
+| 'admin/<controller>' the system will check by the route 
+| '<controller>'.
 |
 */
 
-$config['bundles'] = array(
-	'Backend' => array(
-		'location' => 'BackendBundle',
-		'route' => 'backend',
-		'default' => TRUE
-	)
-);
+$config['bundles'] = array();
 
 ?>
